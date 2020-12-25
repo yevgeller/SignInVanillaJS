409b9678145b902a274fdd78ae8f7f9892ec047f
Eugene Geller <EGeller@chainbridgetech.com>
2020-12-24 23:45:28 -0600

    displaying the queue

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,30 @@ function a() {
     fNameContainer.value = lNameContainer.value = 'abc';
 
     console.log(people);
+    refreshQueue();
+}
+
+function refreshQueue() {
+    let div = document.createElement("div");
+    if (people.length === 0) {
+        div.append("No one in queue");
+    } else {
+        for (var i = 0; i < people.length; i++) {
+            let pDiv = document.createElement("div");
+            pDiv.id = "person" + i;
+            pDiv.innerHTML = people[i].name + ', arrived: ' + people[i].when;
+            div.append(pDiv);
+        }
+    }
+
+    let container = document.getElementById("queueContainer");
+    container.innerHTML = "";
+    container.append(div);
+}
+
+function displayPeople() {
+    if (people.length === 0) {
+        return "<div>No one in the queue</div>";
+    }
+    //let ret = "<div>No"
 }
